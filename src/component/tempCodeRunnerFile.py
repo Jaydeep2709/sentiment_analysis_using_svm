8c9d529b2858f646e0ec9d9ade3ad649f7a1db7c
jaydeep <jaydeepkhot2709@gmail.com>
2023-04-05 19:39:53 +0530

    data_ingestion commit

---
+++ src/component/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+
+# from src.components.data_transformation import DataTransformation
+# from src.components.data_transformation import DataTransformationConfig
+
+# from src.components.model_trainer import ModelTrainerConfig
+# from src.components.model_trainer import ModelTrainer
